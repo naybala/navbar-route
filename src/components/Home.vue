57d1c0549b6bd0e7eae416ffae9fbab78d8432e5
naybala <naybalaismylife.nbl@gmail.com>
2022-05-18 12:00:14 +0630

    commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,16 +2,16 @@
     <div>
         <h5 class="text-primary text-center">This is Home Page</h5>
         <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
-         <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
-          <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
-           <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
-            <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
-             <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
-              <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
-               <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
-                <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
-                 <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
-                 
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, dolore aspernatur! A, deleniti nostrum, harum cum suscipit veritatis quae repellat sunt omnis consequatur asperiores voluptates quo eligendi iste at mollitia?</p>
+
     </div>
 </template>
 
